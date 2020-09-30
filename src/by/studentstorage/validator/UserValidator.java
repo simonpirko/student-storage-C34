68bf94a67b41e4ffca76b53957212110afc8c599
Женя <noximt@gmail.com>
2020-09-30 19:49:20 +0300

    Add UserAction

---
+++ src/by/studentstorage/validator/UserValidator.java
@@ -0,0 +1,25 @@
+package by.studentstorage.validator;
+
+import by.studentstorage.domain.Role;
+
+public class UserValidator {
+    public static boolean validLogin(String login){
+        return login.length() > 3;
+    }
+
+    public static boolean validPassword(String pass){
+        return pass.length() >= 4;
+    }
+
+    public static boolean validName(String name){
+        return name.length() > 0;
+    }
+
+    public static boolean validSurname(String surName){
+        return surName.length() > 0;
+    }
+
+    public static  boolean validRole(String role){
+         return role.equals("STUDENT") || role.equals("ADMIN")|| role.equals("HEADMAN")|| role.equals("TEACHER");
+    }
+}
